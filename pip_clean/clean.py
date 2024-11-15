b530d0d03717aad054c7525b637a4a870f9951e4
jkylekelly <kyle@semgrep.com>
2024-11-15 09:08:51 -0800

    simple print for unused deps

--- pip_clean/clean.py
+++ pip_clean/clean.py
@@ -32,33 +32,32 @@ def get_pyfiles(source_dir: str) -> set[str]:
 def get_code_deps(pyfiles: set[str]) -> set[NormalizedName]:
     imports: set[NormalizedName] = set()
 
+    # adapted from 
     # https://stackoverflow.com/questions/9008451/python-easy-way-to-read-all-import-statements-from-py-module
-    # not currently working fully
     for path in pyfiles:
         with open(path) as fh:        
             root = ast.parse(fh.read(), path)
 
         for node in ast.walk(root):
             if isinstance(node, ast.Import):
-                module = []
+                imports.add(canonicalize_name(node.names[0].name))
             elif isinstance(node, ast.ImportFrom):  
-                module = node.module.split('.') if node.module else []
-            else:
-                continue
-
-            for n in node.names:
-                imports.add(canonicalize_name(n.name))
+                imports.add(canonicalize_name(node.module))
 
     return imports
 
 
 def clean_deps(requirements_file: str, source_dir: str) -> bool:
     # get a list of package names from the requirements.txt
-    requirements = get_requirements_deps(requirements_file)
+    requirements_deps = get_requirements_deps(requirements_file)
     
     # get package names used in code based on ASTs
     pyfiles = get_pyfiles(source_dir)
-    print(get_code_deps(pyfiles))
+    code_deps = get_code_deps(pyfiles)
+
+    missing = list(sorted(requirements_deps - code_deps))
+    print(missing)
+
 
 
 def main():
@@ -69,9 +68,7 @@ def main():
     parser.add_argument('-t', '--target', help='root directory of python project')
     args = parser.parse_args()
     
-    clean_deps(args.requirements, args.target)
-    # parse_deps(args.r)
-    
+    clean_deps(args.requirements, args.target)    
 
 if __name__ == "__main__":
     main()
